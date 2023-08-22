9e83558b2f5773f9eac86200c23577db75d04d69
snahuel99 <sanahuel99@gmail.com>
2023-08-21 21:22:04 -0300

    Cambia el puerto

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.listen(3000, () => console.log("Server Online"));
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get("/", (req,res) => res.sendFile(path.join(__dirname, "views/home.html")));
 app.get("/register", (req,res) => res.sendFile(path.join(__dirname, "views/register.html")));
